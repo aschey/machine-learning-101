b4e61d4f97c27956d159e0162b01cd4c5a6370fb
Austin Schey <aschey13@gmail.com>
2019-12-11 21:13:43 -0600

    add chapter 1

--- chapter1/code/classifier.py
+++ chapter1/code/classifier.py
@@ -1,77 +1,76 @@
+# https://medium.com/machine-learning-101/chapter-1-supervised-learning-and-naive-bayes-classification-part-2-coding-5966f25f1475
 import os
-import numpy as np
+import string
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, LancasterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+import numpy as np
+
+# stopwords = set(stopwords.words('english') + list(string.punctuation))
+# include_subject = True
+
+# def read_data(folder):
+#     data = []
+#     labels = []
+#     files = (os.path.join(folder, f) for f in os.listdir(folder))
+#     p = PorterStemmer()
+#     for fil in files:
+#         with open(fil, 'r') as f:
+#             if include_subject:
+#                 line = ' '.join(f.readlines())
+#             else:
+#                 line = f.readlines()[2]
+#             data.append([p.stem(w) for w in word_tokenize(line) if w not in stopwords and w.isalpha() and len(w) > 1])
+
+#             if "spmsg" in fil:
+#                 labels.append(1)
+#             else:
+#                 labels.append(0)
+#     return data, labels
 
 
-def make_Dictionary(root_dir):
-    all_words = []
-    emails = [os.path.join(root_dir,f) for f in os.listdir(root_dir)]
-    for mail in emails:
-        with open(mail) as m:
-            for line in m:
-                words = line.split()
-                all_words += words
-    dictionary = Counter(all_words)
-    list_to_remove = dictionary.keys()
+# def clean_data(file_data, top):
+#     words = [w for data in file_data for w in data]
+#     if top is not None:
+#         return [k[0] for k in Counter(words).most_common(top)]
+#     return words
 
-    for item in list_to_remove:
-        if item.isalpha() == False:
-            del dictionary[item]
-        elif len(item) == 1:
-            del dictionary[item]
-    dictionary = dictionary.most_common(3000)
+# def extract_features(file_data, words):
+    
+#     features = np.zeros((len(file_data), len(words)))
+#     for i, data in enumerate(file_data):
+#         for j, word in enumerate(data):
+#             try:
+#                 features[i,words.index(word)] = data.count(word)
+#             except:
+#                 continue
+#     return features
 
-    return dictionary
+# train_folder = 'chapter1/train-mails'
+# test_folder = 'chapter1/test-mails'
+# train_data, train_labels = read_data(train_folder)
+# test_data, test_labels = read_data(test_folder)
 
+# count = clean_data(train_data, 2000)
+# train_features = extract_features(train_data, count)
+# test_features = extract_features(test_data, count)
+train_features = np.load('train_features.npy')
+test_features = np.load('test_features.npy')
 
-
-def extract_features(mail_dir):
-    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
-    features_matrix = np.zeros((len(files),3000))
-    train_labels = np.zeros(len(files))
-    count = 0;
-    docID = 0;
-    for fil in files:
-      with open(fil) as fi:
-        for i,line in enumerate(fi):
-          if i == 2:
-            words = line.split()
-            for word in words:
-              wordID = 0
-              for i,d in enumerate(dictionary):
-                if d[0] == word:
-                  wordID = i
-                  features_matrix[docID,wordID] = words.count(word)
-        train_labels[docID] = 0;
-        filepathTokens = fil.split('/')
-        lastToken = filepathTokens[len(filepathTokens) - 1]
-        if lastToken.startswith("spmsg"):
-            train_labels[docID] = 1;
-            count = count + 1
-        docID = docID + 1
-    return features_matrix, train_labels
-
-
-
-TRAIN_DIR = "../train-mails"
-TEST_DIR = "../test-mails"
-
-dictionary = make_Dictionary(TRAIN_DIR)
-
-print "reading and processing emails from file."
-features_matrix, labels = extract_features(TRAIN_DIR)
-test_feature_matrix, test_labels = extract_features(TEST_DIR)
-
+train_labels = np.load('train_labels.npy')
+test_labels = np.load('test_labels.npy')
 
 model = GaussianNB()
 
-print "Training model."
+print("Training model.")
 #train model
-model.fit(features_matrix, labels)
+model.fit(train_features, train_labels)
 
-predicted_labels = model.predict(test_feature_matrix)
+predicted_labels = model.predict(test_features)
 
-print "FINISHED classifying. accuracy score : "
-print accuracy_score(test_labels, predicted_labels)
+print("FINISHED classifying. accuracy score : ")
+print(accuracy_score(test_labels, predicted_labels))
---
+++ clean.py
@@ -0,0 +1,65 @@
+# https://medium.com/machine-learning-101/chapter-1-supervised-learning-and-naive-bayes-classification-part-2-coding-5966f25f1475
+import os
+import string
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, LancasterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from collections import Counter
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+stopwords = set(stopwords.words('english') + list(string.punctuation))
+include_subject = True
+
+def read_data(folder):
+    data = []
+    labels = []
+    files = (os.path.join(folder, f) for f in os.listdir(folder))
+    p = PorterStemmer()
+    for fil in files:
+        with open(fil, 'r') as f:
+            if include_subject:
+                line = ' '.join(f.readlines())
+            else:
+                line = f.readlines()[2]
+            data.append([p.stem(w) for w in word_tokenize(line) if w not in stopwords and w.isalpha() and len(w) > 1])
+
+            if "spmsg" in fil:
+                labels.append(1)
+            else:
+                labels.append(0)
+    return data, labels
+
+
+def clean_data(file_data, top):
+    words = [w for data in file_data for w in data]
+    if top is not None:
+        return [k[0] for k in Counter(words).most_common(top)]
+    return words
+
+def extract_features(file_data, words):
+    
+    features = np.zeros((len(file_data), len(words)))
+    for i, data in enumerate(file_data):
+        for j, word in enumerate(data):
+            try:
+                features[i,words.index(word)] = data.count(word)
+            except:
+                continue
+    return features
+
+train_folder = 'chapter1/train-mails'
+test_folder = 'chapter1/test-mails'
+train_data, train_labels = read_data(train_folder)
+test_data, test_labels = read_data(test_folder)
+
+count = clean_data(train_data, 2000)
+train_features = extract_features(train_data, count)
+test_features = extract_features(test_data, count)
+
+np.save('train_labels', train_labels)
+np.save('test_labels', test_labels)
+np.save('train_features', train_features)
+np.save('test_features', test_features)
